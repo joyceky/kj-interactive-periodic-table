d3e5f003c49998bf48c0ef530bdf05cb10508742
Kyle Joyce <kyyjoyce@gmail.com>
2017-01-20 22:08:15 -0700

    update

--- app/root/app.module.ts
+++ app/root/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PeriodicTable } from '../periodic-table/periodic-table.component';
 import { BohrModel } from '../bohr-model/bohr-model.component';
 
-
 @NgModule({
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   bootstrap: [
     AppComponent,
